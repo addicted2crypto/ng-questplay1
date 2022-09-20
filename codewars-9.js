c1051558f5f0f57e4aa8abac55c0cc30ce3c264f
Willim <tundra2727@gmail.com>
2022-09-19 19:17:51 -0700

    Daily codewars 9th try

---
+++ codewars-9.js
@@ -0,0 +1,12 @@
+DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+// FUNDAMENTALS
+
+
+// My solution:
+
+function doubleInteger(i){
+    i+= i++;
+    return i;
+  }
