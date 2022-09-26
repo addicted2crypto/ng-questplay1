1f9d6be6d5edb4a544d7bab64656ae3664d86b95
Willim <tundra2727@gmail.com>
2022-09-25 20:22:44 -0700

    code wars daily add

---
+++ codewars-10.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// STRINGSFUNDAMENTALS
+
+// my solution
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
